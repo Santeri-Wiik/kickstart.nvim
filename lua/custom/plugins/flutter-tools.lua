4410f70737bd183b12777547056ccc369d8d661c
Santeri Wiik <santeri.wiik@gmail.com>
2025-04-26 13:30:25 +0300

    Make loading of flutter-tools conditional

--- lua/custom/plugins/flutter-tools.lua
+++ lua/custom/plugins/flutter-tools.lua
@@ -1,5 +1,7 @@
+local isFlutter = #vim.fs.find('pubspec.yaml', { upward = true }) > 0
 return {
   'nvim-flutter/flutter-tools.nvim',
+  cond = isFlutter,
   lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
