52561f5e152c8124d3ed130303bc4973a0fa42e9
Santer Wiik <santeri.wiik@gmail.com>
2026-08-23 23:27:56 +0300

    EoD WIP, custom init, tree-sitter-textobjects

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,7 +8,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.diffopt = 'internal,filler,closeoff,linematch:100'
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = '80,120'
 vim.opt.scrolloff = 8
 vim.opt.jumpoptions = 'clean,stack'
 
--- lua/custom/plugins/tree-sitter-textobjects.lua
+++ lua/custom/plugins/tree-sitter-textobjects.lua
@@ -1,13 +1,12 @@
-return {
-  'nvim-treesitter/nvim-treesitter-textobjects',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
-  textobjects = {
-    select = {
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-      },
-    },
-  },
+do return end
+
+vim.pack.add { {
+  src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+  version = 'main',
+} }
+
+require('nvim-treesitter-textobjects').setup{
+  select = {
+    lookahead = true
+  }
 }
