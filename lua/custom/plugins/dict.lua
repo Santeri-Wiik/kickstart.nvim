3a559828cd3d9784bd8fc24de8d3080cf6ea27e2
Santer Wiik <santeri.wiik@gmail.com>
2026-08-26 17:29:20 +0300

    replace external dict plugin with local function (stolen from reddit)

--- lua/custom/plugins/dict.lua
+++ lua/custom/plugins/dict.lua
@@ -1,11 +1,43 @@
-vim.pack.add({{
-    src = 'https://github.com/jalvesaq/dict.nvim'
-}})
+local function define_word()
+   local word = vim.fn.expand("<cword>")
+   local buffer = vim.api.nvim_create_buf(false, true)
+   local lines = nil
 
-require'dict'.setup({
-    dict = 'wn',
-    dict_dir = '/usr/share/dictd',
-    cache_dir = vim.env.HOME .. '/.cache/dict.nvim',
-})
+   if vim.fn.executable('dict') == 0 then
+      vim.notify("dict executable not found", "ERROR")
+      return
+   end
 
-vim.keymap.set('n', '<Leader>d', '<Cmd>lua require("dict").lookup()<CR>')
+   local command = vim.system({ "dict", word }, { text = true }):wait()
+
+   if command.code ~= 0 then
+      print(command.stderr)
+      return
+   end
+
+   lines = vim.split(command.stdout, "\n", { plain = true })
+   vim.api.nvim_buf_set_lines(buffer, 0, -1, false, lines)
+
+   local window = vim.api.nvim_open_win(buffer, true, {
+      relative = "cursor",
+      bufpos = { 0, 0 },
+      border = "single",
+      width = 90,
+      height = 25,
+      style = "minimal",
+      title = word
+   })
+
+   vim.api.nvim_create_autocmd("WinLeave", {
+      buf = buffer,
+      once = true,
+      callback = function()
+         vim.api.nvim_win_close(window, true)
+         vim.bo[buffer].bufhidden = "wipe"
+      end
+   })
+
+   print("Dictionary", word)
+end
+
+vim.keymap.set("n", "<leader>d", define_word, { desc = "Dictionary" })
